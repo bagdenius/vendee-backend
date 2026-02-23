c7931e8a19285741b11a403513dfaf39190e4325
bagdenius <bagdensparrow@gmail.com>
2026-02-23 02:54:06 +0200

    setup config service in main.ts / setup cookie parser and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+  const configService = app.get(ConfigService);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [configService.getOrThrow<string>('CLIENT_URL')],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+  await app.listen(configService.getOrThrow<number>('SERVER_PORT') ?? 3000);
 }
 bootstrap();
