dd09805b98e244dfc7c94fa10a3133c27af947a3
bagdenius <bagdensparrow@gmail.com>
2026-02-23 02:45:13 +0200

    define infra module and setup basic app module / fix env import in prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { InfraModule } from './infra/infra.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), InfraModule],
 })
 export class AppModule {}
